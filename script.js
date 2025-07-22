84819bd1d348b0d4c323a98e40ed107c1636ead3
Camily notebook <camily17eduarda@gmail.com>
2025-07-21 23:25:16 -0300

    add carrossel na habilidade

--- script.js
+++ script.js
@@ -2,3 +2,32 @@ function virarCarta() {
     const carta = document.querySelector('.carta-container');
     carta.classList.toggle('virada');
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const faixa = document.querySelector('.carrossel-faixa');
+    const blocos = document.querySelectorAll('.habilidade-bloco');
+    const btnEsquerda = document.querySelector('.seta-esquerda');
+    const btnDireita = document.querySelector('.seta-direita');
+
+    let indiceAtual= 0;
+
+    function mostrarSlide(indice) {
+        const largura = blocos[0].clientWidth;
+        faixa.style.transform = `translateX(-${indice * largura}px)`;
+    }
+
+    btnDireita.addEventListener('click', () =>{
+        if (indiceAtual <blocos.length - 1) {
+            indiceAtual++;
+            mostrarSlide(indiceAtual);
+        }
+    });
+
+    btnEsquerda.addEventListener('click',() => {
+        if (indiceAtual > 0) {
+            indiceAtual--;
+            mostrarSlide(indiceAtual);
+        }
+    });
+    window.addEventListener('resize', () => mostrarSlide(indiceAtual));
+});
