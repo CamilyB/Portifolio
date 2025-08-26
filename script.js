83fbaee280550b256be74ff51946a2d4d01075fb
CamilyB <camily17eduarda@gmail.com>
2025-08-25 22:40:46 -0300

    add funcionalidade a projetos

--- script.js
+++ script.js
@@ -31,3 +31,26 @@ document.addEventListener("DOMContentLoaded", function() {
     });
     window.addEventListener('resize', () => mostrarSlide(indiceAtual));
 });
+
+function abrirModal(modal1, modal2, modal3, modal4) {
+    document.getElementById(modal1).style.display = 'block';
+    document.getElementById(modal2).style.display = 'block';
+    document.getElementById(modal3).style.display = 'block';
+    document.getElementById(modal4).style.display = 'block';
+}
+
+function fecharModal(modal1, modal2, modal3, modal4) {
+    document.getElementById(modal1).style.display = 'none';
+    document.getElementById(modal2).style.display = 'none';
+    document.getElementById(modal3).style.display = 'none';
+    document.getElementById(modal4).style.display = 'none';
+}
+
+window.onclick = function(event) {
+    const modais = document.getElementsByClassName("modal");
+    for (let i = 0; i< modais.length; i++) {
+        if (event.target == modais[i]) {
+            modais[i].style.display = "none";
+        }
+    }
+}
